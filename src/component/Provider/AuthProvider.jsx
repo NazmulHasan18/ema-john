8be17d6b5bf6db6844fe80f93f5f2ceb25d3b934
NazmulHasan18 <nazmul182218@gmail.com>
2023-04-19 16:56:57 +0600

    login with google fixed'

--- src/component/Provider/AuthProvider.jsx
+++ src/component/Provider/AuthProvider.jsx
@@ -36,6 +36,7 @@ const AuthProvider = ({ children }) => {
       return signOut(auth);
    };
    const googleSignIn = () => {
+      setLoading(true);
       return signInWithPopup(auth, googleProvider);
    };
 
